d40283f8c8965f93d0b6895d17a1469d02b84bf6
Daniel Martins <daniel.martins50@gmail.com>
2026-04-19 19:22:17 +0100

    Move selector & fix size

--- app/components/selector/selector.tsx
+++ app/components/selector/selector.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import PreviewSlider from "../ui/preview-slider/preview-slider";
+import "./selector.css";
 
 export default function Selector({
   options,
@@ -10,22 +12,24 @@ export default function Selector({
   const [activeComponent, setActiveComponent] = useState("Line Waves");
 
   return (
-    <div className="absolute top-4 left-4 righ-4 w-full min-h-20 p-8 flex row  border-solid border-neutral-300 opacity-0 hover:opacity-100 transition-opacity duration-300 ">
-      <div className="flex grow row rounded-xl overflow-hidden bg-neutral-900/9">
-        {options.map((name, index) => {
-          return (
-            <button
-              key={name}
-              className={`flex grow border-r-2 p-4 border-neutral-900/50 border-r-neutral-50/5 hover:bg-neutral-900/70 font-no text-2xl transition-colors justify-center items-center cursor-pointer ${activeComponent === name ? "bg-neutral-900/70" : ""}`}
-              onClick={() => {
-                setActiveComponent(name);
-                onChangeChoice(name);
-              }}
-            >
-              {name}
-            </button>
-          );
-        })}
+    <div className="absolute bottom-0 left-0 right-0 w-screen min-h-20 flex column p-4">
+      <div className="flex grow column border border-solid border-neutral-50/40  opacity-0 hover:opacity-100 transition-opacity duration-300 bg-neutral-900/95 rounded-xl">
+        <div className="flex grow row rounded-xl overflow-hidden selector-shadow">
+          {options.map((name, index) => {
+            return (
+              <button
+                key={name}
+                className={`flex grow border-solid border-r-2 p-8  border-r-neutral-50/50 hover:bg-neutral-50/30 text-xl transition-colors justify-center items-center cursor-pointer ${activeComponent === name ? "bg-neutral-50/20" : ""}`}
+                onClick={() => {
+                  setActiveComponent(name);
+                  onChangeChoice(name);
+                }}
+              >
+                {name}
+              </button>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
